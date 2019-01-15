1963b2d7f4f577061bc8ccf0afe2383dcb7b7e66
gsdavis1959 <43298320+gsdavis1959@users.noreply.github.com>
2019-01-14 19:28:12 -0600

    Delete W - pivot.table.r

--- W - pivot.table.r
+++
@@ -1,4 +0,0 @@
-library(pivottabler)
-# arguments:  qhpvt(dataFrame, rows, columns, calculations, ...)
-names(bhmtrains)
-qhpvt(bhmtrains, "TOC", "TrainCategory", "n()") # TOC = Train Operating Company 
