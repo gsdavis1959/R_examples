0fadfcb5339292159da59d01c760363d4767cd7b
gsdavis1959 <43298320+gsdavis1959@users.noreply.github.com>
2019-03-27 18:53:35 -0500

    Add files via upload

--- R.for.Data.Science.Book.r
+++ R.for.Data.Science.Book.r
@@ -124,3 +124,95 @@ bar <- ggplot(data = diamonds) +
 
 bar + coord_flip()
 bar + coord_polar()
+
+
+# error bars
+source('~/Data/RStatistics/WindowsRSource/Useful Functions/summarySE.r')
+tg <- ToothGrowth
+head(tg)
+# summarySE provides the standard deviation, standard error of the mean, and a (default 95%) confidence interval
+tgc <- summarySE(tg, measurevar="len", groupvars=c("supp","dose"))
+tgc
+
+# Standard error of the mean
+ggplot(tgc, aes(x=dose, y=len, colour=supp)) + 
+  geom_errorbar(aes(ymin=len-se, ymax=len+se), width=.1) +
+  geom_line() +
+  geom_point()
+
+
+# The errorbars overlapped, so use position_dodge to move them horizontally
+pd <- position_dodge(0.1) # move them .05 to the left and right
+
+ggplot(tgc, aes(x=dose, y=len, colour=supp)) + 
+  geom_errorbar(aes(ymin=len-se, ymax=len+se), width=.1, position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd)
+
+
+# Use 95% confidence interval instead of SEM
+ggplot(tgc, aes(x=dose, y=len, colour=supp)) + 
+  geom_errorbar(aes(ymin=len-ci, ymax=len+ci), width=.1, position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd)
+
+# Black error bars - notice the mapping of 'group=supp' -- without it, the error
+# bars won't be dodged!
+ggplot(tgc, aes(x=dose, y=len, colour=supp, group=supp)) + 
+  geom_errorbar(aes(ymin=len-ci, ymax=len+ci), colour="black", width=.1, position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd, size=3)
+ggplot(tgc, aes(x=dose, y=len, colour=supp, group=supp)) + 
+  geom_errorbar(aes(ymin=len-se, ymax=len+se), colour="black", width=.1, position=pd) +
+  geom_line(position=pd) +
+  geom_point(position=pd, size=3, shape=21, fill="white") + # 21 is filled circle
+  xlab("Dose (mg)") +
+  ylab("Tooth length") +
+  scale_colour_hue(name="Supplement type",    # Legend label, use darker colors
+                   breaks=c("OJ", "VC"),
+                   labels=c("Orange juice", "Ascorbic acid"),
+                   l=40) +                    # Use darker colors, lightness=40
+  ggtitle("The Effect of Vitamin C on\nTooth Growth in Guinea Pigs") +
+  expand_limits(y=0) +                        # Expand y range
+  scale_y_continuous(breaks=0:20*4) +         # Set tick every 4
+  theme_bw() +
+  theme(legend.justification=c(1,0),
+        legend.position=c(1,0))               # Position legend in bottom right
+
+# bar charts
+# Use dose as a factor rather than numeric
+tgc2 <- tgc
+tgc2$dose <- factor(tgc2$dose)
+
+# Error bars represent standard error of the mean
+ggplot(tgc2, aes(x=dose, y=len, fill=supp)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=len-se, ymax=len+se),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
+
+
+# Use 95% confidence intervals instead of SEM
+ggplot(tgc2, aes(x=dose, y=len, fill=supp)) + 
+  geom_bar(position=position_dodge(), stat="identity") +
+  geom_errorbar(aes(ymin=len-ci, ymax=len+ci),
+                width=.2,                    # Width of the error bars
+                position=position_dodge(.9))
+
+# with titles and legend
+ggplot(tgc2, aes(x=dose, y=len, fill=supp)) + 
+  geom_bar(position=position_dodge(), stat="identity",
+           colour="black", # Use black outlines,
+           size=.3) +      # Thinner lines
+  geom_errorbar(aes(ymin=len-se, ymax=len+se),
+                size=.3,    # Thinner lines
+                width=.2,
+                position=position_dodge(.9)) +
+  xlab("Dose (mg)") +
+  ylab("Tooth length") +
+  scale_fill_hue(name="Supplement type", # Legend label, use darker colors
+                 breaks=c("OJ", "VC"),
+                 labels=c("Orange juice", "Ascorbic acid")) +
+  ggtitle("The Effect of Vitamin C on\nTooth Growth in Guinea Pigs") +
+  scale_y_continuous(breaks=0:20*4) +
+  theme_bw()
