19f92f0a34562466495390389da4bb67d550a74f
gsdavis1959 <43298320+gsdavis1959@users.noreply.github.com>
2019-05-09 18:01:31 -0500

    Add files via upload

---
+++ spread.columns.r
@@ -0,0 +1,16 @@
+library(tidyverse)
+df <- data.frame(month=rep(1:3,2),
+                 student=rep(c("Amy", "Bob"), each=3),
+                 A=c(9, 7, 6, 8, 6, 9),
+                 B=c(6, 7, 8, 5, 6, 7))
+
+df
+
+df %>% 
+  gather(variable, value, -(month:student)) %>%
+  unite(temp, student, variable) %>%
+  spread(temp, value)
+
+library(data.table) ## v >= 1.9.6
+dcast(setDT(df), month ~ student, value.var = c("A", "B")) 
+
