c6131fdb94411a64ecfddefadc3ebf92f21c0897
gsdavis1959 <43298320+gsdavis1959@users.noreply.github.com>
2019-06-16 14:42:45 -0500

    Add files via upload

---
+++ model.down.r
@@ -0,0 +1,49 @@
+# Prepare the data
+library(modelDown)
+library("DALEX")
+titanic <- na.omit(titanic)
+
+# Random Forest model
+library("randomForest")
+model_titanic_rf <- randomForest(survived == "yes" ~ gender + age + class + embarked +
+                                   fare + sibsp + parch,  data = titanic[,-5])
+explain_titanic_rf <- explain(model_titanic_rf, 
+                              data = titanic[,-c(9,5)],
+                              y = titanic$survived == "yes", 
+                              label = "Random Forest v7")
+
+# SVM model
+library("e1071")
+model_titanic_svm <- svm(survived == "yes" ~ class + gender + age + sibsp +
+                           parch + fare + embarked, data = titanic[,-5], 
+                         type = "C-classification", probability = TRUE)
+explain_titanic_svm <- explain(model_titanic_svm, data = titanic[,-c(9,5)], 
+                               y = titanic$survived == "yes", 
+                               label = "Support Vector Machines")
+
+# GBM model
+library("gbm")
+model_titanic_gbm <- gbm(survived == "yes" ~ class + gender + age + sibsp +
+                           parch + fare + embarked, data = titanic[,-5], n.trees = 15000)
+explain_titanic_gbm <- explain(model_titanic_gbm, data = titanic[,-c(9,5)], 
+                               y = titanic$survived == "yes", 
+                               predict_function = function(m,x) predict(m, x, n.trees = 15000, type = "response"),
+                               label = "Generalized Boosted Models")
+
+# k-NN model
+
+library("caret")
+model_titanic_knn <- knn3(survived == "yes" ~ class + gender + age + sibsp +
+                            parch + fare + embarked, data = titanic[,-5], k = 5)
+explain_titanic_knn <- explain(model_titanic_knn, data = titanic[,-c(9,5)], 
+                               y = titanic$survived == "yes", 
+                               predict_function = function(m,x) predict(m, x)[,2],
+                               label = "k-Nearest Neighbours")
+
+
+# Website generation
+modelDown(explain_titanic_rf, explain_titanic_gbm, 
+          explain_titanic_svm, explain_titanic_knn,
+          device = "svg",
+          remote_repository_path = "MI2DataLab/modelDown_example/docs",
+          output_folder = "modelDown_Titanic_example")
