b09dca297786bd6262c238d9dc3c407f18b9bac8
Greg Davis <43298320+gsdavis1959@users.noreply.github.com>
2020-02-26 20:54:04 -0600

    Add files via upload

--- animation.r
+++ animation.r
@@ -10,6 +10,7 @@ ggplot(datasaurus_dozen, aes(x=x, y=y))+
 
 library(plotly)
 library(gapminder)
+head(gapminder)
 p <- gapminder %>%
   plot_ly(
     x = ~gdpPercap, 
@@ -56,3 +57,53 @@ ggplot(gapminder, aes(gdpPercap, lifeExp, size = pop, colour = country)) +
 
 # Save at gif:
 anim_save("271-ggplot2-animated-gif-chart-with-gganimate2.gif")
+
+
+accumulate_by <- function(dat, var) {
+  var <- lazyeval::f_eval(var, dat)
+  lvls <- plotly:::getLevels(var)
+  dats <- lapply(seq_along(lvls), function(x) {
+    cbind(dat[var %in% lvls[seq(1, x)], ], frame = lvls[[x]])
+  })
+  dplyr::bind_rows(dats)
+}
+
+d <- txhousing 
+fig <- d %>%
+  filter(year > 2005, city %in% c("Abilene", "Bay Area"))
+fig <- fig %>% accumulate_by(~date)
+
+
+fig <- fig %>%
+  plot_ly(
+    x = ~date, 
+    y = ~median,
+    split = ~city,
+    frame = ~frame, 
+    type = 'scatter',
+    mode = 'lines', 
+    line = list(simplyfy = F)
+  )
+fig <- fig %>% layout(
+  xaxis = list(
+    title = "Date",
+    zeroline = F
+  ),
+  yaxis = list(
+    title = "Median",
+    zeroline = F
+  )
+) 
+fig <- fig %>% animation_opts(
+  frame = 100, 
+  transition = 0, 
+  redraw = FALSE
+)
+fig <- fig %>% animation_slider(
+  hide = T
+)
+fig <- fig %>% animation_button(
+  x = 1, xanchor = "right", y = 0, yanchor = "bottom"
+)
+
+fig
