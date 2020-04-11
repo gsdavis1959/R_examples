9e11e5510d7121af45d92cfb49f28e8b575217fe
Greg Davis <43298320+gsdavis1959@users.noreply.github.com>
2020-04-10 20:24:34 -0500

    Add files via upload

--- Plotly.Basic.Bar.Chart.r
+++ Plotly.Basic.Bar.Chart.r
@@ -1,5 +1,13 @@
 library(tidyverse)
 library(plotly)
-
+mpg
 # multiple traces (less performant, but more interactive)
 plot_ly(diamonds, x = ~cut, color = ~clarity, colors = "Accent")
+
+# stacked bar chart
+
+fig <- plot_ly(mpg, x = ~trans, y = ~cty, type = 'bar', name = 'City')
+fig <- fig %>% add_trace(y = ~hwy, name = 'Highway')
+fig <- fig %>% layout(yaxis = list(title = 'Mile per Gallon'), barmode = 'stack')
+
+fig
