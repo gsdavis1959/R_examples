a1b918cf5673cb8239347dae5435689c7332b30c
Greg Davis <43298320+gsdavis1959@users.noreply.github.com>
2022-05-02 18:33:33 -0500

    Add files via upload

---
+++ explorr.r
@@ -0,0 +1,12 @@
+# Install release version from CRAN
+install.packages("xplorerr")
+library(shinythemes)
+library(xplorerr)
+df <- data("mtcars")
+
+app_vistributions()
+app_inference()
+app_linear_regression()
+app_logistic_regression()
+app_rfm_analysis()
+app_visualizer()
